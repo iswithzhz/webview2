1481e35a2b65732e86205767ea3c75841b4c9bde
iswithzhz <iswith@126.com>
2025-02-16 13:24:42 +0800

    Delete Simplified Chinese version directory

--- Simplified Chinese version/RegAsm.bat
+++
@@ -1,5 +0,0 @@
-	@ECHO OFF
-	cd /d %~dp0
-	"C:\Windows\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe" /codebase "WebView2.dll" /tlb "WebView2.tlb"
-	PAUSE
-	CLS
--- Simplified Chinese version/main.prg
+++
@@ -1,8 +0,0 @@
-If _vfp.StartMode <> 0 Then 
-   Erase ( Addbs( Getenv("TEMP") ) + "*.fxp" )
-Endif 
-   
-Do Form »ù´¡ä¯ÀÀ°æ.scx
-Read Events
- 
- 
